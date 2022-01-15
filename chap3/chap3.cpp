275078df38b51444c647659cf06a96a20cd64983
robin burri <robin@pop-os.localdomain>
2022-01-15 10:24:45 +0100

    chap 3 started

---
+++ chap3/chap3.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+
+using std::cin;     using std::setprecision;
+using std::cout;     using std::string;
+using std::endl;     using std::streamsize;
+
+int main()
+{
+    /**
+     *? Ask student name
+     */
+    
+}
+
