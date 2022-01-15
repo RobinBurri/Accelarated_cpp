1158e2fcaa1cde7be05dfb293ed40761ac7d2639
Robin Burri <robinburri@tutanota.com>
2022-01-15 17:00:17 +0100

    working on chap3

--- chap3/chap3.cpp
+++ chap3/chap3.cpp
@@ -2,16 +2,43 @@
 #include <ios>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;     using std::setprecision;
 using std::cout;     using std::string;
 using std::endl;     using std::streamsize;
+using std::vector;
 
 int main()
 {
-    /**
-     *? Ask student name
-     */
-    
+     // ask for and read the student's name
+	string name;
+	cout << "What's your name?: ";
+	cin >> name;
+    cout << name + ", ";
+	double midterm_grade, final_grade;
+	cout << "what's your midterm and final grades?: ";
+	cin >> midterm_grade >> final_grade;
+	cout << "Enter all your homework grade(s), "
+			"followed by end-of-file: " << endl;
+    int count = 0;
+    double sum =  0;
+    // a variable into which to read
+    double x;
+    vector<double> homework;
+
+    // // we have read count grades so far, and
+    // // sum is the sum of the first count grades
+    while (cin >> x) {
+        homework.push_back(x);
+		count++;
+		sum += x;
+    }
+	//  // write the result
+    streamsize prec = cout.precision();
+    // cout
+    cout 	<< "Your final grade is " << setprecision(3)
+			<< 0.2 * midterm_grade + 0.4 * final_grade + 0.4 * sum / count
+			<< setprecision(prec) << endl;
 }
 
